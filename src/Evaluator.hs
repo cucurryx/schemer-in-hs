f6c19f16d4424e2454349bb6f870fe291b1c7f71
xiebei1108 <xiebei1108@outlook.com>
2019-02-25 23:46:01 +0800

    finish basic primitive calculator
    Change-Id: I1aad204cc944a5e4c82ca43f5230805bb3c80935

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -2,19 +2,6 @@ module Evaluator where
 
 import Parser
 
--- lisp ADT
--- data LispVal = Atom String                      -- atom
---              | List [LispVal]                   -- (x y z)
---              | DottedList [LispVal] LispVal     -- (x y . z)
---              | Number Integer                   -- 1
---              | String String                    -- "this is string"
---              | Bool Bool                        -- #t / #f
---              | Character Char                   -- 'a'
---              | Float Double                     -- 1.22
---              | Ratio Rational                   -- 1/2  
---              | Vector (Array Int LispVal)       -- #(1 2 3)
-
-
 -- evaluator 
 eval :: LispVal -> LispVal
 eval val@(String _) = val
@@ -24,3 +11,43 @@ eval val@(Character _) = val
 eval val@(Float _) = val
 eval val@(Ratio _) = val
 eval (List [Atom "quoted", val]) = val
+eval (List (Atom func:args)) = call func $ map eval args
+
+-- call function(the first argument) with args(the second argument)
+-- if can't get a function from primitives, return Bool False
+-- otherwise, apply this function to args
+call :: String -> [LispVal] -> LispVal
+call func args = maybe (Bool False) (call' args) $ lookup func primitives
+    where call' args f = f args
+    
+-- wrapper for haskell primitive functions
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [
+    ("+", basicFuncWrapper (+)),
+    ("-", basicFuncWrapper (-)),
+    ("*", basicFuncWrapper (*)),
+    ("/", basicFuncWrapper div),
+    ("mod", basicFuncWrapper mod),
+    ("quotient", basicFuncWrapper quot),
+    ("remainder", basicFuncWrapper rem)]
+
+basicFuncWrapper :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+basicFuncWrapper func args =  Number $ foldl1 func params
+    where params = map unwrapNum args
+
+-- unwrap number from LispVal
+unwrapNum :: LispVal -> Integer
+unwrapNum (Number x) = x
+unwrapNum _ = 0
+
+symbol :: LispVal -> LispVal
+symbol (Atom _) = Bool True
+symbol _ = Bool False
+
+number :: LispVal -> LispVal
+number (Number _) = Bool True
+number _ = Bool False
+
+string :: LispVal -> LispVal
+string (String _) = Bool True
+string _ = Bool False
